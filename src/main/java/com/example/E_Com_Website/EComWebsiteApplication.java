9e3ed433fdffb4a232ba6998ad9d4b94387941af
yuvrajpatilyp <yuvraj_sp.starsoftech@outlook.com>
2025-11-06 11:33:06 +0530

    Initial commit

---
+++ src/main/java/com/example/E_Com_Website/EComWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_Com_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/E_Com_Website/EComWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_Com_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
