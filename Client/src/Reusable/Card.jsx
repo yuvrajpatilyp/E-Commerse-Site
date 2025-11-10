02a16a6bded427e3a806aa9aba41bc5e69cdaae6
yuvrajpatilyp <yuvraj_sp.starsoftech@outlook.com>
2025-11-10 11:46:32 +0530

    added the eye and heart icons and also the discount

--- Client/src/Reusable/Card.jsx
+++ Client/src/Reusable/Card.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { Heart, Eye } from "lucide-react";
 
-const Card = ({ title, newprice, oldprice, image }) => {
+const Card = ({ title, newprice, oldprice, image, discount }) => {
   return (
-    <div className="flex flex-col w-64">
+    <div className="flex flex-col items-center w-64 relative">
+      <div className="bg-gray-100 p-6 w-full h-60 shadow-md rounded-lg relative overflow-hidden hover:shadow-lg transition-transform duration-300 hover:scale-105 flex items-center justify-center">
+        
+        // discount add it will only appers only when discount prop is passed fro that we used &&
+        {discount && (
+          <div className="absolute top-2 left-2 bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded">
+            {discount}
+          </div>
+        )}
 
-      <div className="bg-gray-100 p-10 w-full h-60 shadow-md transform transition-transform duration-300 hover:scale-105 flex items-center justify-center rounded-lg">
+        
         <img src={image} alt={title} className="object-contain h-40" />
+
+
+        <div className="absolute top-2 right-2 flex flex-col space-y-2">
+          <button className="bg-white p-1.5 rounded-full shadow hover:bg-gray-200">
+            <Heart size={16} className="text-gray-700" />
+          </button>
+          <button className="bg-white p-1.5 rounded-full shadow hover:bg-gray-200">
+            <Eye size={16} className="text-gray-700" />
+          </button>
+        </div>
       </div>
 
-      <h2 className="text-lg font-semibold mt-3 ">{title}</h2>
+     //title and price 
+      <h2 className="text-lg font-semibold mt-3 text-center">{title}</h2>
 
-      <div className="flex space-x-4">
+      <div className="flex space-x-3">
         <h1 className="text-red-500 font-semibold">{newprice}</h1>
         <h1 className="text-gray-400 line-through">{oldprice}</h1>
       </div>
