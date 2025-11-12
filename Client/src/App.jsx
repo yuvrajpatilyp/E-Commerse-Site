b5e8c0479d07aa63ba3d8e51bd608ad3faadecdf
yuvrajpatilyp <yuvraj_sp.starsoftech@outlook.com>
2025-11-12 15:19:34 +0530

    added routing to rout

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import TopHeader from './Reusable/TopHeader'
+import NavigationBar from './Reusable/NavigationBar'
+import Footer from './Reusable/Footer'
 
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-    
+      <TopHeader/>
+
+      <NavigationBar isLoggedIn={isLoggedIn} />
+
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pageNavigator" element={<PageNavigator />} />
+
+      
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+      </Routes>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App
