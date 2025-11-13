b13191ea65333a44a1f84ec4bbfb3453080b599d
Pooja-pisal-2003 <pooja_bp.starsoftech@outlook.com>
2025-11-13 15:58:39 +0530

    Update code

---
+++ Client/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Routes,Route } from 'react-router-dom'
+import TopHeader from './Reusable/TopHeader'
+import NavigationBar from './Reusable/NavigationBar'
+import Footer from './Reusable/Footer'
+import About from './AboutPage/About'
+import Contact from './ContactPage/Contact'
+import PageNavigator from './Reusable/PageNavigator'
+import Login from './Login'
+ 
+ 
+function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      <TopHeader/>
+ 
+      <NavigationBar isLoggedIn={isLoggedIn} />
+ 
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/pageNavigator" element={<PageNavigator />} />
+ 
+     
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+      </Routes>
+ 
+      <Footer />
+    </div>
+  );
+}
+ 
+export default App
